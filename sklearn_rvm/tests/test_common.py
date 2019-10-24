1c1971f69fe675212cfc64cc91f7cd14dbc7891f
Warvito <ianonimato@hotmail.com>
2019-10-24 12:18:24 +0100

    Add EMRVR on unit tests, remove specific unit tests

--- sklearn_rvm/tests/test_common.py
+++ sklearn_rvm/tests/test_common.py
@@ -2,10 +2,10 @@ import pytest
 
 from sklearn.utils.estimator_checks import check_estimator
 
-from sklearn_rvm import RVR, RVC
+from sklearn_rvm import RVR, RVC, EMRVR
 
 @pytest.mark.parametrize(
-    "Estimator", [RVR, RVC]
+    "Estimator", [RVR, RVC, EMRVR]
 )
 def test_all_estimators(Estimator):
     return check_estimator(Estimator)
