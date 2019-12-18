47a45fbd53a35905b785f3b7d959b488b617b5d6
Warvito <ianonimato@hotmail.com>
2019-12-18 11:42:49 +0000

    Remove all unittests

--- sklearn_rvm/tests/test_common.py
+++ sklearn_rvm/tests/test_common.py
@@ -1,11 +1,1 @@
 import pytest
-
-from sklearn.utils.estimator_checks import check_estimator
-
-from sklearn_rvm import EMRVR
-
-@pytest.mark.parametrize(
-    "Estimator", [EMRVR]
-)
-def test_all_estimators(Estimator):
-    return check_estimator(Estimator)
