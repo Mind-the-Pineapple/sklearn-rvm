e19117daaa358f10af53bac1ed138300af13bce9
Warvito <walhugolp@gmail.com>
2020-01-21 23:06:25 +0000

    update doc

--- doc/conf.py
+++ doc/conf.py
@@ -63,7 +63,7 @@ autodoc_default_options = {
 }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # generate autosummary even if no references
 autosummary_generate = True
@@ -171,7 +171,7 @@ html_favicon = 'logos/favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
